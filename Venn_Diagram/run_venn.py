dbe144635354246cccc21605f474f4cba863d536
KRSaraswat <126743540+KRSaraswat@users.noreply.github.com>
2025-10-02 16:44:04 -0500

    Update and rename run_venn.py to Venn_Diagram/run_venn.py

--- Venn_Diagram/run_venn.py
+++ Venn_Diagram/run_venn.py
@@ -5,7 +5,7 @@ set2 = {'C', 'D', 'F', 'G'}
 set3 = {'B', 'D', 'G', 'H', 'I', 'J'}
 
 plot_venn3(set1, set2, set3,
-           labels=['E-State', 'MFP', 'ABOCH'],
+           labels=['Set1', 'Set2', 'Set3'],
            colors=['#E41A1C', '#377EB8', '#FFAB52'],
            bold_labels=True,
            save_path="venn_final.png")
